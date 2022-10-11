090dbfce3b9195391a7754e5141759dea7306cf4
Aram <armsir-8182@mail.ru>
2022-10-11 19:42:57 +0300

    My third program in C#

---
+++ Exercise_03_sum/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Написать программу в С#,который в входе получет от пользователя 2 целые числа, 
+а на выходе сложит их и покажет результат на экране. */
+Console.WriteLine("Enter the first number a= ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the second number b= ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("The summa of two numbers a + b = ");
+Console.Write(a+b);
