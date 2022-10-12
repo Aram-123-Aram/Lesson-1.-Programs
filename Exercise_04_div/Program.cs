b643762c4c317b93ec055fea6e8619e71f10e49c
Aram <armsir-8182@mail.ru>
2022-10-12 03:48:33 +0300

    My thourth Program in C#

---
+++ Exercise_04_div/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Написать программу,который на вход принимает 2 целые числа,
+ а на выход дает результат деление первого числа на вторую */
+ Console.Write("Enter the first number a= ");
+ double a = int.Parse(Console.ReadLine()!);
+ Console.Write("Enter the second number b= ");
+ double b = int.Parse(Console.ReadLine()!);
+ Console.Write("The result a:b= " + a/b);
