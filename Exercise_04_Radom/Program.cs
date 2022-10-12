62559be8735099eb163d6706b8d52d212b90e537
Aram <armsir-8182@mail.ru>
2022-10-12 04:35:52 +0300

    Program Random

---
+++ Exercise_04_Radom/Program.cs
@@ -0,0 +1,6 @@
+﻿// Сгенерируем 2 случайные целые число, выводим результат их сложение.
+int a = new Random().Next(1,20); // 1,2,3,4...18,19
+Console.WriteLine("Генерируем первое число a= " + a);
+int b = new Random().Next(1,10);
+Console.WriteLine("Генерируем второе число b= " + b);
+Console.Write("The result is- a:b= " + a/b);
