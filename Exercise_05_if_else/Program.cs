3c965c1f9001d4e1f55f4377c3323e4998a4af89
Aram <armsir-8182@mail.ru>
2022-10-12 13:51:28 +0300

    using operator if___else ___

---
+++ Exercise_05_if_else/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Задача: Запросить у пользователья вводить свою имя. Если пользователя завут "Маша",
+выводить сообщение: "Ура! Это наша Маша!".
+В остольних случаях выводить сообщение: "Привет _имя пользователя_" */
+Console.Write("Enter your name: ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{Console.WriteLine("Ура!Это же наша " + username + "!"); }
+else {Console.Write("Привет " + username +"!"); } 
